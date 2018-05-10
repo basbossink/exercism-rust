83c481b47627a5d44a61be8c1b0ac7ac69d7c28a
Bas Bossink <bas.bossink@divverence.com>
2018-05-11 00:06:19 +0200

    Implement is_lea_year.

--- leap/src/lib.rs
+++ leap/src/lib.rs
@@ -1,3 +1,6 @@
-pub fn is_leap_year(year: i32) -> bool {
-    unimplemented!("true if {} is a leap year", year)
-}
+pub fn is_leap_year(year: i32) -> bool {
+    fn is_divisible_by(number: i32, divisor: i32) -> bool {
+        number % divisor == 0
+    }
+    is_divisible_by(year, 4) && (!is_divisible_by(year, 100) || is_divisible_by(year, 400))
+}
