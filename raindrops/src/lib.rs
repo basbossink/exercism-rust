8ed695f51d8d10bb78f941027b095251e1b0b764
Bas Bossink <bas.bossink@divverence.com>
2018-05-11 00:26:56 +0200

    Finish raindrops problem.

--- raindrops/src/lib.rs
+++ raindrops/src/lib.rs
@@ -1,3 +1,16 @@
-pub fn raindrops(n: usize) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
-}
+pub fn raindrops(n: usize) -> String {
+    fn has_factor(number: usize, divisor: usize) -> bool {
+        number % divisor == 0
+    }
+
+    match n {
+        x if has_factor(x, 3 * 5 * 7) => "PlingPlangPlong".to_string(),
+        x if has_factor(x, 5 * 7) => "PlangPlong".to_string(),
+        x if has_factor(x, 3 * 7) => "PlingPlong".to_string(),
+        x if has_factor(x, 3 * 5) => "PlingPlang".to_string(),
+        x if has_factor(x, 7) => "Plong".to_string(),
+        x if has_factor(x, 5) => "Plang".to_string(),
+        x if has_factor(x, 3) => "Pling".to_string(),
+        x => x.to_string(),
+    }
+}
