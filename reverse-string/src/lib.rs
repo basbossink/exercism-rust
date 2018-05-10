c156d753f0e8ac5922b9e3cde8e3377299d6b9e5
Bas Bossink <bas.bossink@divverence.com>
2018-05-11 01:30:59 +0200

    Solve reverse string problem using recursion.

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,3 +1,13 @@
 pub fn reverse(input: &str) -> String {
-    unimplemented!("Write a function to reverse {}", input);
+    fn reverse_recursively(input: &str, accumulator: String) -> String {
+        let mut iterator = input.chars();
+        match iterator.next() {
+            Some(character) => reverse_recursively(
+                &iterator.collect::<String>(),
+                character.to_string() + &accumulator,
+            ),
+            None => accumulator,
+        }
+    }
+    reverse_recursively(input, String::new())
 }
