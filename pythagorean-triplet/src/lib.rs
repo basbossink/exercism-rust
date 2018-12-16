d348e4fe4b61f522e3595c313fe3d3544f569f3b
Bas Bossink <bas.bossink@divverence.com>
2018-12-16 21:11:55 +0100

    Finish Pythagorean triplet exercise

--- pythagorean-triplet/src/lib.rs
+++ pythagorean-triplet/src/lib.rs
@@ -1,5 +1,16 @@
 use std::collections::HashSet;
 
 pub fn find(sum: u32) -> HashSet<[u32; 3]> {
-    unimplemented!("Given the sum {}, return all possible Pythagorean triplets, which produce the said sum, or an empty HashSet if there are no such triplets. Note that you are expected to return triplets in [a, b, c] order, where a < b < c", sum);
+    let mut result = HashSet::new();
+    for a in 1..sum / 2 {
+        let a_squared = a * a;
+        for b in a + 1..((sum - a) / 2) + 1 {
+            let sum_of_sides = a_squared + b * b;
+            let c = sum - a - b;
+            if sum_of_sides == c * c {
+                result.insert([a, b, c]);
+            }
+        }
+    }
+    result
 }
