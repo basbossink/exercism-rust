d2fd2b5ea6e372ae3736c180437f6f9056713766
Bas Bossink <bas.bossink@divverence.com>
2018-12-16 19:34:57 +0100

    Finish prime-factor exercise

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -1,3 +1,17 @@
 pub fn factors(n: u64) -> Vec<u64> {
-    unimplemented!("This should calculate the prime factors of {}", n)
+    let mut result: Vec<u64> = Vec::new();
+    let mut rest = n;
+    let mut divisor = 2;
+    while divisor <= rest {
+        while divides(rest, divisor) {
+            result.push(divisor);
+            rest = rest / divisor;
+        }
+        divisor = divisor + 1
+    }
+    result
+}
+
+fn divides(lhs: u64, rhs: u64) -> bool {
+    lhs % rhs == 0
 }
