3cd62c8661050a6ae11adfba3c312e53ad7c2fcc
Bas Bossink <bas.bossink@divverence.com>
2018-12-29 00:51:17 +0100

    Solve beer song exercise

--- beer-song/src/lib.rs
+++ beer-song/src/lib.rs
@@ -1,7 +1,23 @@
+const FIRST_VERSE : &'static str = "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n";
+const SECOND_VERSE : &'static str = "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n";
+
 pub fn verse(n: i32) -> String {
-    unimplemented!("emit verse {}", n)
+    match n {
+        0 => FIRST_VERSE.to_string(),
+        1 => SECOND_VERSE.to_string(),
+        _ => format!(
+            "{} bottles of beer on the wall, {} bottles of beer.\nTake one down and pass it around, {} bottle{} of beer on the wall.\n",
+            n,
+            n,
+            n-1,
+            if n > 2 { "s" } else { "" }),
+    }
 }
 
 pub fn sing(start: i32, end: i32) -> String {
-    unimplemented!("sing verses {} to {}, inclusive", start, end)
+    (end..=start)
+        .rev()
+        .map(|i| verse(i))
+        .collect::<Vec<String>>()
+        .join("\n")
 }
