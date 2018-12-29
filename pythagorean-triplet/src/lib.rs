f5583d687c3d4a9565124473ff52663c4f68cfc7
Bas Bossink <bas.bossink@divverence.com>
2018-12-29 15:47:48 +0100

    a should not be greater than sum divided by 3 iso 2

--- pythagorean-triplet/src/lib.rs
+++ pythagorean-triplet/src/lib.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 
 pub fn find(sum: u32) -> HashSet<[u32; 3]> {
     let mut result = HashSet::new();
-    for a in 1..sum / 2 {
+    for a in 1..sum / 3 {
         let a_squared = a * a;
         for b in a + 1..((sum - a) / 2) + 1 {
             let sum_of_sides = a_squared + b * b;
